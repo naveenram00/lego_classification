541e527f4a19db063d8d2753fda7e1e0ae7ee4bc
tylerlian <tylerlian@gmail.com>
2018-07-05 15:29:15 -0700

    added opencv test and edited screenshot

--- assembly_segmentation/screen.py
+++ assembly_segmentation/screen.py
@@ -5,8 +5,22 @@
 
 
 import pygame
-size1= 960, 500
+import pygame.camera
+#import VideoCapture
+from pygame.locals import *
+
+
+pygame.init
+pygame.camera.init()
+pygame.display.init()
+pygame.camera.list_cameras()
+if pygame.display.get_init() == True:
+    print("1")
+    
+
+size1= 200, 200
 print (pygame.FULLSCREEN)
+pygame.display.toggle_fullscreen()
 #
-pygame.display.set_mode(size1, pygame.FULLSCREEN)
+pygame.display.set_mode(size1)
 
