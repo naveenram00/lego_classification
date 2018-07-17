302cef4a5b890a2c50a16e69edb79c6a002d00be
tylerlian <tylerlian@gmail.com>
2018-07-17 12:52:01 -0700

    Order mode working

--- assembly_segmentation/assembly_segmentation.py
+++ assembly_segmentation/assembly_segmentation.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 import math
+global nodes
 nodes = []
 select_radius = 15
 objects = []
@@ -10,6 +11,7 @@ class Node:
         self.x = x_init
         self.y = y_init
         self.is_selected = False
+        self.next_node = None
     
     def deselect(self):
        self.is_selected = False
@@ -28,6 +30,9 @@ class Node:
             pygame.draw.circle(screen, (0, 255, 255), (self.x, self.y), 6, 0)
         else:
             pygame.draw.circle(screen, (0, 5, 255), (self.x, self.y), 4, 0)
+        if self.next_node != None:
+            pygame.draw.line(screen, (50, 5, 255), (self.x, self.y), self.next_node.get_pos(), 3)
+            
 
     def __repr__(self):
         return "".join(["Node(", str(self.x), ",", str(self.y), ")"])
@@ -37,13 +42,18 @@ def distance(p0, p1):
     return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
 
 def select_node(pos, screen):
-    
-    for i in range(len(nodes)):
-        nodes[i].deselect()
-        if distance(pos, nodes[i].get_pos()) < select_radius:
+    selected = None
+    print(nodes)
+    for node in nodes:
+        node.deselect()
+        print(distance(pos, node.get_pos()))
+        if distance(pos, node.get_pos()) < select_radius:
             #set selected to nearest node
-            selected = nodes[i]
+            selected = node
             selected.select()
+            print("test")
+    return selected
+           
 
             
 
@@ -51,7 +61,7 @@ def node_selection():
     # Initialize game and create a screen object.
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
-    modes = ["create", "select", "move"]
+    modes = ["create", "select", "order"]
     mode_index = 0
     select_radius = 50
     n = 0
@@ -81,17 +91,29 @@ def node_selection():
                     #pygame.draw.circle(screen, BLUE, (nodes[n]), 4, 0) #Draws a circle at the mouse position!
                     #print(circ[n])
                     #n += 1
-                    print("test")
+                    
                                    
                 if mode == "select":
+                    for node in nodes:
+                        print(node.is_selected)
                     select_node(pygame.mouse.get_pos(), screen)
                          
-                #if mode == "move":
+                if mode == "order":
+                    selected = 0
+                    for node in nodes:
+                        if node.is_selected:
+                            selected = node
+                    if selected == 0:
+                        select_node(pygame.mouse.get_pos(), screen)
+                    else:
+                        selected.next_node = select_node(pygame.mouse.get_pos(), screen)
+                        
                     
                                    
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     mode_index += 1
+                    print("Mode: " + modes[mode_index % len(modes)])
                 if event.key == pygame.K_BACKSPACE or event.key == pygame.K_DELETE:
                     for node in nodes:
                         if node.is_selected:
