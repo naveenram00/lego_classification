217bb470c7ffab747cd103ed0fb50934697aac17
tylerlian <tylerlian@gmail.com>
2018-08-08 10:32:10 -0700

    happy it work

--- assembly_segmentation/python_files/try.py
+++ assembly_segmentation/python_files/try.py
@@ -5,6 +5,7 @@ import numpy as np
 import sys
 import os
 import pygameMenu
+from Screenshotter import cropper
 from pygameMenu.locals import *
 import math
 from PIL import Image, ImageChops
@@ -133,7 +134,7 @@ def message_display(text):
     
 #-----------------------------------------------
 
-def buttons(screen, mode):
+def buttons(screen):
     largeText = pygame.font.Font('freesansbold.ttf', 50)
     smallText = pygame.font.Font("freesansbold.ttf",20)
     mouse = pygame.mouse.get_pos()
@@ -208,23 +209,25 @@ def first_sequence(screen):
         
 #-----------------------------------------------
 
-def second_sequence(screen, mode_index, nodes):
+def second_sequence(screen, mode_index, nodes, modes):
     pygame.display.set_caption("Node Selection")
     background = pygame.image.load("crop.jpg")
     imagerect = background.get_rect()
     screen = pygame.display.set_mode((640, 530))   # This line makes the stuttering 
     screen.fill(WHITE)
-    screen.blit(background, imagerect)
+    #screen.blit(background, imagerect)
     n = 0
     select_radius = 60
     selected = []
     ending = True
+
     while ending == True:
-        screen.fill(WHITE)
-        screen.blit(background, imagerect)
+        
+        
         largeText = pygame.font.Font('freesansbold.ttf', 50)
-        smallText = pygame.font.Font("freesansbold.ttf",20)
+        smallText = pygame.font.Font("freesansbold.ttf", 20)
         mouse = pygame.mouse.get_pos()
+        pygame.draw.rect(screen, WHITE,(0,480,530,50))
         if 0 < mouse[0] < 100 and 480+50 > mouse[1] > 480:
             pygame.draw.rect(screen, L_PURPLE,(0,480,100,50))
         else:
@@ -279,7 +282,7 @@ def second_sequence(screen, mode_index, nodes):
                     
                     if mode == "create":
 
-                        nodes.append(Node(x_init=pygame.mouse.get_pos()[0], y_init=pygame.mouse.get_pos()[1])) #Gets the mouse position
+                        nodes.append(pygame.mouse.get_pos()) #Gets the mouse position
                         print(nodes[n])
                         pygame.draw.circle(screen, BLUE, (nodes[n]), 4, 0) #Draws a circle at the mouse position!
                         
@@ -287,8 +290,8 @@ def second_sequence(screen, mode_index, nodes):
 
 
                     if mode == "select":
-                        for node in nodes:
-                            print(node.is_selected)
+#                         for node in nodes:
+#                             print(node.is_selected)
                         select_node(pygame.mouse.get_pos(), screen)
 
                     if mode == "order":
@@ -457,7 +460,201 @@ def init2():
 init1()            
 first_sequence(screen)
 #init2()
-second_sequence(screen, mode_index, nodes)
+pygame.display.set_caption("Node Selection")
+background = pygame.image.load("crop.jpg")
+imagerect = background.get_rect()
+screen = pygame.display.set_mode((640, 530))   # This line makes the stuttering 
+screen.fill(WHITE)
+#screen.blit(background, imagerect)
+n = 0
+select_radius = 60
+selected = []
+ending = True
+
+while ending == True:
+
+
+    largeText = pygame.font.Font('freesansbold.ttf', 50)
+    smallText = pygame.font.Font("freesansbold.ttf", 20)
+    mouse = pygame.mouse.get_pos()
+    pygame.draw.rect(screen, WHITE,(0,480,530,50))
+    if 0 < mouse[0] < 100 and 480+50 > mouse[1] > 480:
+        pygame.draw.rect(screen, L_PURPLE,(0,480,100,50))
+    else:
+        pygame.draw.rect(screen, PURPLE,(0,480,100,50))
+    if 540 < mouse[0] < 640 and 480+50 > mouse[1] > 480:
+        pygame.draw.rect(screen, L_PURPLE,(540,480,100,50))
+    else:
+        pygame.draw.rect(screen, PURPLE,(540,480,100,50)) 
+
+    TextSurf, TextRect = text_objects(modes[mode_index%len(modes)].upper(), largeText, BLACK)
+
+    TextRect.center = ((640/2),(506))
+    screen.blit(TextSurf, TextRect)    
+    textSurf, textRect = text_objects("<", smallText, WHITE)
+    textRect.center = ( (0+(100/2)), (480+(50/2)) )
+    screen.blit(textSurf, textRect)
+    textSurf, textRect = text_objects(">", smallText, WHITE)
+    textRect.center = ( (540+(100/2)), (480+(50/2)) )
+    screen.blit(textSurf, textRect)
+    mode = modes[mode_index % len(modes)]
+    #set mouse type
+    if mode == "move":
+        pygame.mouse.set_cursor(*pygame.cursors.broken_x)
+    elif mode == "select":
+        pygame.mouse.set_cursor(*pygame.cursors.diamond)
+    elif mode == "order":
+        pygame.mouse.set_cursor(*pygame.cursors.arrow)
+    else:
+        pygame.mouse.set_cursor(*pygame.cursors.tri_left)
+    events = pygame.event.get()    
+    for event in events:
+
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+
+        if event.type == pygame.MOUSEBUTTONDOWN: #This checks for the mouse press event
+
+
+            if mouse[1] > 480:
+
+                if 0 < mouse[0] < 100:
+
+                    print("Mode: " + modes[mode_index % len(modes)])
+                    mode_index = abs(mode_index - 1)
+                    print(mode_index)
+                elif 540 < mouse[0] < 640 :    
+                    print("Mode: " + modes[mode_index % len(modes)])
+                    mode_index += 1
+
+            else:
+
+                if mode == "create":
+
+                    nodes.append(Node(x_init=pygame.mouse.get_pos()[0], y_init=pygame.mouse.get_pos()[1])) #Gets the mouse position
+                    #pygame.draw.circle(screen, BLUE, (nodes[n]), 4, 0) #Draws a circle at the mouse position!
+                    #print(circ[n])
+                    #n += 1
+
+
+                if mode == "select":
+                    for node in nodes:
+                        print(node.is_selected)
+                    select_node(pygame.mouse.get_pos(), screen)
+
+                if mode == "order":
+                    selected = 0
+                    for node in nodes:
+                        if node.is_selected:
+                            selected = node
+                    if selected == 0:
+                        select_node(pygame.mouse.get_pos(), screen)
+                    else:
+                        selected.next_node = select_node(pygame.mouse.get_pos(), screen)
+
+                if mode == "move":
+
+                    node_selected = False
+
+                    for node in nodes:
+                        if node.is_selected:
+                            node_selected = True
+                    if not node_selected:
+                        select_node(pygame.mouse.get_pos(), screen)
+                    else:
+
+                        for node in nodes:
+                            if node.is_selected:
+
+                                node.move_to(pygame.mouse.get_pos())
+                                node.deselect()
+
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                mode_index += 1
+                print("Mode: " + modes[mode_index % len(modes)])
+            elif event.key == pygame.K_LEFT:
+                mode_index = abs(mode_index-1)
+                print("Mode: " + modes[mode_index % len(modes)])
+            if event.key == pygame.K_BACKSPACE or event.key == pygame.K_DELETE:
+                for node in nodes:
+                    if node.is_selected:
+
+                        nn = node.next_node
+                        pn = None
+
+                        for node2 in nodes:
+                            if node2.next_node == node:
+                                pn = node2
+                        if (nn == None and pn == None):
+                            nodes.remove(node)
+                        elif ((not nn == None) and pn == None):
+                            node.next_node = None
+                            nodes.remove(node)
+                            nn.is_selected = True
+                        elif ((not pn == None) and nn == None):
+                            pn.next_node = None
+                            nodes.remove(node)
+                        else:
+                            pn.next_node = nn
+                            nodes.remove(node)
+                            nn.is_selected = True
+            if event.key == pygame.K_ESCAPE:
+                menu2.enable()
+            if event.key == pygame.K_RETURN:
+                print(nodes)
+#                     for node in nodes:
+#                         if node.next_node == None:
+#                             nodes.remove(node)
+#                     print(nodes)
+
+                nodes_copy = [x for x in nodes if not x.next_node == None]
+
+                print(nodes_copy) 
+                for node in nodes_copy:
+                    print(node.next_node)
+
+                ordered_nodes = [nodes_copy[0].get_pos()]
+                #nodes_copy = nodes
+                node = nodes_copy[0]
+                while len(nodes_copy) > 0:
+                    ordered_nodes.append(node.next_node.get_pos())
+                    nodes_copy.remove(node)
+                    node = node.next_node
+                del nodes[:]
+                nodes.extend(nodes_copy)
+
+
+#                     print(ordered_nodes)
+
+                cropper(ordered_nodes)
+                background = pygame.image.load("out.png")
+
+                sys.path.append("/usr/src/lego_classification/part_recognition")
+                import lego_class_module as classifier
+                part_number = classifier.classify("out.png")[0]
+                print ("--------------")
+                print ("Part Number: " + str(part_number))
+                print("Part: " + parts[part_number])
+                print("Repair Instructions: " + instructions[part_number])
+
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    screen.fill((255,255,255))
+    screen.blit(background, imagerect)
+    buttons(screen)
+    for node in nodes:
+        #print(screen)
+        node.draw(screen)
+        
+    menu2.mainloop(events)
+    pygame.display.flip()
+    pygame.display.update()
+    
 #-----------------------------------------------
 
 # except KeyboardInterrupt:
